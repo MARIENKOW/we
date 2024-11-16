4b5f7fc51621049bf4ddbb33fbb9b22b4db36d69
MARIENKOW <den.marienkow@gmail.com>
2024-11-16 22:27:11 +0200

    fix

---
+++ js/app.js
@@ -0,0 +1,60 @@
+const spoilerUpFlex = (target) => {
+   target.parentElement.classList.remove('_spoilerOpen');
+   target.style.height = '0px';
+}
+const spoilerDownFlex = (target) => {
+   target.parentElement.classList.add('_spoilerOpen')
+   let height = target.scrollHeight;
+   target.style.height = `${height}px`;
+}
+const spoilerToggle = (element) => {
+   if (!element.parentElement.classList.contains('_spoilerOpen')) {
+   return spoilerDownFlex(element);
+   } else {
+   return spoilerUpFlex(element);
+   }
+}
+function spoilerClick() {
+   event.preventDefault();
+   if (this.classList.contains('_spoilerRun')) {
+         spoilerToggle(this.nextElementSibling);
+   }
+}
+function spoilerAdd(element){
+   element.nextElementSibling.style.cssText=`height:0px;overflow:hidden;transition:.2s;`
+   element.style.cssText=`cursor:pointer;`
+   element.parentElement.classList.remove('_spoilerOpen');
+   if(!element.parentElement.classList.contains("_spoilerActive")){
+         element.parentElement.classList.add("_spoilerActive");
+         element.classList.add('_spoilerRun')
+         element.addEventListener('click',spoilerClick)
+   }
+}
+function spoilerRemove(element){
+   element.parentElement.classList.remove('_spoilerOpen');
+   element.nextElementSibling.style.cssText=``
+   element.style.cssText=``
+   element.parentElement.classList.remove("_spoilerActive");
+   element.classList.remove('_spoilerRun')
+   element.removeEventListener('click',spoilerClick)
+}
+function spoilerFlex() {
+   const spoilers = document.querySelectorAll("[data-spoiler]");
+   if(spoilers.length>0){
+         for(const spoiler of spoilers){
+               if(spoiler.dataset.spoiler){
+                     if(window.innerWidth<=spoiler.dataset.spoiler){
+                           spoilerAdd(spoiler)
+                     }else{
+                           spoilerRemove(spoiler);
+                     }
+               }else{
+                     spoilerAdd(spoiler)
+               }
+         }
+   }
+}
+
+
+spoilerFlex();
+window.addEventListener('resize',spoilerFlex);
